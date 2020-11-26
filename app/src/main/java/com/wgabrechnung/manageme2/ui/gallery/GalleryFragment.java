225e399f401bdb2a2fe8678a7db539f9d33f9625
Julian Desktop <julianpaschetag@web.de>
2020-11-26 21:21:14 +0100

    floating buttons für Funktionen

--- app/src/main/java/com/wgabrechnung/manageme2/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/wgabrechnung/manageme2/ui/gallery/GalleryFragment.java
@@ -1,11 +1,17 @@
 package com.wgabrechnung.manageme2.ui.gallery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,9 +21,11 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.wgabrechnung.manageme2.CORE_HELPER;
 import com.wgabrechnung.manageme2.HTTP_REQUEST;
 import com.wgabrechnung.manageme2.R;
+import com.wgabrechnung.manageme2.ViewAnimation;
 import com.wgabrechnung.manageme2.adapter.KontoAdapter;
 import com.wgabrechnung.manageme2.adapter.ProjekteAdapter;
 import com.wgabrechnung.manageme2.database.DatabaseKonto;
@@ -28,6 +36,7 @@ import java.util.HashMap;
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    boolean isRotate = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,28 +44,28 @@ public class GalleryFragment extends Fragment {
                 new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
-        Button kontenrundruf  = root.findViewById(R.id.btnKontoRundruf);
-        Button btnKontoDelete  = root.findViewById(R.id.btnKontoDelete);
+//        Button kontenrundruf  = root.findViewById(R.id.btnKontoRundruf);
+//        Button btnKontoDelete  = root.findViewById(R.id.btnKontoDelete);
+//
+//        kontenrundruf.setOnClickListener(v -> {
+//
+//            HashMap<String,String> URLparam = new HashMap<>();
+//            URLparam.put("MODE","2");
+//            String strURL = CORE_HELPER.CREATE_URL(URLparam);
+//
+//            HTTP_REQUEST http_request = new HTTP_REQUEST(root.getContext(),2);
+//            http_request.execute(strURL);
+//
+//        });
+//
+//        btnKontoDelete.setOnClickListener(v -> {
+//
+//            DatabaseKonto db = new DatabaseKonto(root.getContext());
+//            db.deleteContents();
+//
+//        });
 
-        kontenrundruf.setOnClickListener(v -> {
-
-            HashMap<String,String> URLparam = new HashMap<>();
-            URLparam.put("MODE","2");
-            String strURL = CORE_HELPER.CREATE_URL(URLparam);
-
-            HTTP_REQUEST http_request = new HTTP_REQUEST(root.getContext(),2);
-            http_request.execute(strURL);
-
-        });
-
-        btnKontoDelete.setOnClickListener(v -> {
-
-            DatabaseKonto db = new DatabaseKonto(root.getContext());
-            db.deleteContents();
-
-        });
-
-
+        //Hier wird ie Liste aufgebaut
         final RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
 
         recyclerView.setHasFixedSize(true);
@@ -78,6 +87,46 @@ public class GalleryFragment extends Fragment {
         projekte.add(kl2);
 
         recyclerView.setAdapter(new ProjekteAdapter(projekte));
+
+        //Die kleinen fabs werden ausgeblendet
+        FloatingActionButton l1 = root.findViewById(R.id.fabMic);
+        FloatingActionButton l2 = root.findViewById(R.id.fabCall);
+
+        ViewAnimation.init(l1);
+        ViewAnimation.init(l2);
+
+        //Hier wir der ActionButton aufgebaut
+        final FloatingActionButton actionButton = root.findViewById(R.id.floatingActionButton);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                isRotate = ViewAnimation.rotateFab(v,!isRotate,actionButton);
+                if(isRotate){
+                    ViewAnimation.showIn(l1);
+                    ViewAnimation.showIn(l2);
+                }else{
+                    ViewAnimation.showOut(l1);
+                    ViewAnimation.showOut(l2);
+                }
+            }
+        });
+
+        l1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hier können die anderen Fragmente geöffnet werden
+            }
+        });
+
+        l2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Hier können die anderen Fragmente geöffnet werden
+            }
+        });
+
+
 
         return root;
     }
