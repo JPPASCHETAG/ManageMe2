02fbb3c8e46fa9d2a1907f84141d793e31c06b00
Julian Desktop <julianpaschetag@web.de>
2020-11-28 14:20:43 +0100

    FABs im Konto eingebaut

--- app/src/main/java/com/wgabrechnung/manageme2/ui/konto/HomeFragment.java
+++ app/src/main/java/com/wgabrechnung/manageme2/ui/konto/HomeFragment.java
@@ -1,31 +1,40 @@
 package com.wgabrechnung.manageme2.ui.konto;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.wgabrechnung.manageme2.CORE_HELPER;
+import com.wgabrechnung.manageme2.HTTP_REQUEST;
 import com.wgabrechnung.manageme2.R;
+import com.wgabrechnung.manageme2.ViewAnimation;
 import com.wgabrechnung.manageme2.adapter.KontoAdapter;
 import com.wgabrechnung.manageme2.database.DatabaseKonto;
+import com.wgabrechnung.manageme2.ui.newProjekt.NewProjektFragment;
+import com.wgabrechnung.manageme2.ui.slideshow.SlideshowFragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
 
-    ArrayList arrayList;
-    ArrayAdapter arrayAdapter;
+    private boolean isRotate = false;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,7 +48,77 @@ public class HomeFragment extends Fragment {
         DatabaseKonto dbKont = new DatabaseKonto(root.getContext());
         ArrayList<String[]> umsaetze = dbKont.getAllText();
 
-        recyclerView.setAdapter(new KontoAdapter(umsaetze));
+        KontoAdapter kontoAdapter = new KontoAdapter(umsaetze);
+        recyclerView.setAdapter(kontoAdapter);
+
+        //Die kleinen fabs werden ausgeblendet
+        FloatingActionButton kontenRundruf = root.findViewById(R.id.kontenRundruf);
+        FloatingActionButton autoSort = root.findViewById(R.id.autoSort);
+
+        ViewAnimation.init(kontenRundruf);
+        ViewAnimation.init(autoSort);
+
+        //Hier wir der ActionButton aufgebaut
+        final FloatingActionButton actionButton = root.findViewById(R.id.floatingActionButtonKonto);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                isRotate = ViewAnimation.rotateFab(v,!isRotate,actionButton);
+                if(isRotate){
+                    ViewAnimation.showIn(kontenRundruf);
+                    ViewAnimation.showIn(autoSort);
+                }else{
+                    ViewAnimation.showOut(kontenRundruf);
+                    ViewAnimation.showOut(autoSort);
+                }
+            }
+        });
+
+        kontenRundruf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(root.getContext());
+                builder.setCancelable(true);
+                builder.setTitle("Kontenrundruf starten");
+                builder.setMessage("Möchten Sie den Kontenrundruf starten und alle aktuellen Umsätze in die App importieren?");
+                builder.setPositiveButton("starten", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Toast.makeText(root.getContext(),"Kontenrundruf gestartet",Toast.LENGTH_SHORT).show();
+
+                            HashMap<String,String> URLparam = new HashMap<String,String>();
+                            URLparam.put("MODE","2");
+                            String strURL = CORE_HELPER.CREATE_URL(URLparam);
+
+                            HTTP_REQUEST http_request = new HTTP_REQUEST(root.getContext(),2);
+                            http_request.execute(strURL);
+
+                        }
+                    });
+                builder.setNegativeButton("abbrechen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(root.getContext(),"Kontenrundruf abgebrochen",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
+        autoSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
+
+
 
 
         return root;
